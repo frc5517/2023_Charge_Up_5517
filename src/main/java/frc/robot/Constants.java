40c6148adb23db80433ad36b07882fa1caff3914
KonnorR <konnor0601@gmail.com>
2023-03-04 20:24:59 -0500

    3/4/2023
    Added Comments throughout, vision, intake pinch, intakeLift, intakeExtender, and updated repos.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,7 +14,8 @@ package frc.robot;
  */
 public final class Constants {
     public static final class OIConstants {
-        public static final int xboxController = 0;
+        public static final int drController = 0;
+        public static final int opJoystick = 1;
     }
     public static final class DriveConstants {
         public static final int rightVictorID0 = 0;
@@ -23,5 +24,14 @@ public final class Constants {
         public static final int leftVictorID3 = 3;
         public static final int leftVictorID4 = 4;
         public static final int leftTalonID5 = 5;
+    }
+    public static final class ManipulatorConstants {
+        public static final int rotationSpark = 6;
+        public static final int extenderSpark = 7;
+        public static final int pinchSolenoidForward = 31;
+        public static final int pinchSolenoidReverse = 32;
+    }
+    public static final class VisionConstants {
+        public final static String camera = "Camera";
     }
 }
